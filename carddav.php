64a0009f0f0e6ba107f00497eed4a50752ffa24e
Benjamin Schieder <blindcoder@scavenger.homeip.net>
2011-04-30 09:57:03 +0000

    code cleanup, indention, make size of url input field as long as the value, but at least 40

--- carddav.php
+++ carddav.php
@@ -70,61 +70,71 @@ class carddav extends rcube_plugin
   }}}
 
   // user preferences
-  function cd_preferences($args) {
-    if($args['section'] == 'cd_preferences') {
-      $this->add_texts('localization/', false);
-      $rcmail = rcmail::get_instance();
+  function cd_preferences($args)
+  {{{
+	if($args['section'] != 'cd_preferences')
+		return;
+	$this->add_texts('localization/', false);
+	$rcmail = rcmail::get_instance();
+	
+	$prefs = $rcmail->config->get('carddav', array());
+	
+	// check box for username
+	$use_carddav = $prefs['use_carddav'];
+	$checkbox = new html_checkbox(array('name' => '_cd_use_carddav', 'value' => 1));
+	$content_use_carddav = $checkbox->show($prefs['use_carddav']?1:0);
 
-      $prefs = $rcmail->config->get('carddav', array());
+	// input box for username
+	$input = new html_inputfield(array('name' => '_cd_username', 'type' => 'text', 'autocomplete' => 'off', 'value' => $prefs['username']));
+	$content_username = $input->show();
 
-      // check box for username
-      $use_carddav = $prefs['use_carddav'];
-      $checkbox = new html_checkbox(array('name' => '_cd_use_carddav', 'value' => 1));
-      $content_use_carddav = $checkbox->show($prefs['use_carddav']?1:0);
-      // input box for username
-      $input = new html_inputfield(array('name' => '_cd_username', 'type' => 'text', 'autocomplete' => 'off', 'value' => $prefs['username']));
-      $content_username = $input->show();
-      // input box for username
-      $input = new html_inputfield(array('name' => '_cd_password', 'type' => 'password', 'autocomplete' => 'off', 'value' => $prefs['password']));
-      $content_password = $input->show();
-      // input box for URL
-      $input = new html_inputfield(array('name' => '_cd_url', 'type' => 'text', 'autocomplete' => 'off', 'value' => $prefs['url'], 'size'=>'40'));
-      $content_url = $input->show();
+	// input box for username
+	$input = new html_inputfield(array('name' => '_cd_password', 'type' => 'password', 'autocomplete' => 'off', 'value' => $prefs['password']));
+	$content_password = $input->show();
 
-      $args['blocks']['cd_preferences'] =  array(
-        'options' => array(
-                           array('title'=> Q($this->gettext('cd_use_carddav')), 'content' => $content_use_carddav), 
-                           array('title'=> Q($this->gettext('cd_username')), 'content' => $content_username), 
-                           array('title'=> Q($this->gettext('cd_password')), 'content' => $content_password),
-                           array('title'=> Q($this->gettext('cd_url')), 'content' => $content_url),
-                          ),
-        'name'    => Q($this->gettext('cd_title'))
-        );
-    } 
-    return($args);
-  }
+	// input box for URL
+	$size = isset($prefs['url']) ? strlen($prefs['url']) : 40;
+	$input = new html_inputfield(array('name' => '_cd_url', 'type' => 'text', 'autocomplete' => 'off', 'value' => $prefs['url'], 'size' => $size < 40 ? 40 : $size));
+	$content_url = $input->show();
+	
+	$args['blocks']['cd_preferences'] = array(
+		'options' => array(
+			array('title'=> Q($this->gettext('cd_use_carddav')), 'content' => $content_use_carddav), 
+			array('title'=> Q($this->gettext('cd_username')), 'content' => $content_username), 
+			array('title'=> Q($this->gettext('cd_password')), 'content' => $content_password),
+			array('title'=> Q($this->gettext('cd_url')), 'content' => $content_url),
+		),
+		'name' => Q($this->gettext('cd_title'))
+	);
+	return($args);
+  }}}
 
   // add a section to the preferences tab
-  function cd_preferences_section($args) {
-    $this->add_texts('localization/', false);
-    $args['list']['cd_preferences'] = array(
-      'id'      => 'cd_preferences',
-      'section' => Q($this->gettext('cd_title'))
-      );
-    return($args);
-  }
+  function cd_preferences_section($args)
+  {{{
+	$this->add_texts('localization/', false);
+	$args['list']['cd_preferences'] = array(
+		'id'      => 'cd_preferences',
+		'section' => Q($this->gettext('cd_title'))
+	);
+	return($args);
+  }}}
 
   // save preferences
-  function cd_save($args) {
-    if($args['section'] != 'cd_preferences') return;
+  function cd_save($args)
+  {{{
+	if($args['section'] != 'cd_preferences')
+		return;
 
-    $rcmail = rcmail::get_instance();
+	$rcmail = rcmail::get_instance();
 
-    $prefs = array('use_carddav' => isset($_POST['_cd_use_carddav']) ? 1 : 0,
-                   'username' => get_input_value('_cd_username', RCUBE_INPUT_POST),
-                   'password' => get_input_value('_cd_password', RCUBE_INPUT_POST),
-                   'url' => get_input_value('_cd_url', RCUBE_INPUT_POST));
-    $args['prefs']['carddav'] = $prefs;
-    return($args);
-  }
+	$prefs = array(
+		'use_carddav' => isset($_POST['_cd_use_carddav']) ? 1 : 0,
+		'username' => get_input_value('_cd_username', RCUBE_INPUT_POST),
+		'password' => get_input_value('_cd_password', RCUBE_INPUT_POST),
+		'url' => get_input_value('_cd_url', RCUBE_INPUT_POST)
+	);
+	$args['prefs']['carddav'] = $prefs;
+	return($args);
+  }}}
 }
