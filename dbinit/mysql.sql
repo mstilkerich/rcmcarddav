835a615f817ddf6af2ffd5e9ba72a01f1f83ed00
Michael Stilkerich <ms@mike2k.de>
2011-11-05 11:24:14 +0000

    table definition for local storage of CardDAV contacts
    The scheme is close to the contacts table of the regular roundcube
    addressbook. It contains the entire vcard and additionally some
    attributes for direct database filtering that are commonly used in
    searches (I guess;-)).
    
    Also fixed the addressbook table scheme a bit.

--- dbinit/mysql.sql
+++ dbinit/mysql.sql
@@ -4,8 +4,21 @@ CREATE TABLE IF NOT EXISTS carddav_addressbooks (
  	name VARCHAR(64) NOT NULL,
  	username VARCHAR(64) NOT NULL,
  	password VARCHAR(64) NOT NULL,
- 	url VARCHAR(256) NOT NULL,
+ 	url VARCHAR(255) NOT NULL,
  	active TINYINT UNSIGNED NOT NULL DEFAULT 1,
  	user_id INT(10) UNSIGNED NOT NULL REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
- 	last_updated TIMESTAMP
+ 	last_updated TIMESTAMP DEFAULT 0,
+);
+
+CREATE TABLE IF NOT EXISTS carddav_contacts (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	abook_id INT UNSIGNED NOT NULL REFERENCES carddav_addressbooks(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	name VARCHAR(255),  -- display name, or <FirstName> <LastName> if not set
+	email VARCHAR(255), -- ", " separated list of mail addresses
+	firstname VARCHAR(255),
+	surname VARCHAR(255),
+	vcard LONGTEXT,     -- complete vcard
+	words text,         -- search keywords
+	etag VARCHAR(255),  -- entity tag, can be used to check if card changed on server
+	cuid VARCHAR(255)   -- unique identifier of the card within the collection
 );
