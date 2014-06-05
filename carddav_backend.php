cf55e91256dec251aa0fa6b33a96b99ace100d86
brendan braybrook <brendan@tucows.com>
2014-06-05 17:49:28 +0000

    fix null nickname condition. fix strpos test

--- carddav_backend.php
+++ carddav_backend.php
@@ -750,11 +750,11 @@ EOF
 
 		} else { // individual/other
       if (trim($save_data['name']) == '') { // roundcube display fix for contacts that don't have first/last names
-        if (trim($save_data['nickname'] !== '')) {
+        if ($save_data['nickname'] !== NULL && trim($save_data['nickname'] !== '')) {
           $save_data['name'] = $save_data['nickname'];
         } else {
           foreach ($save_data as $key=>$val) {
-            if (strpos($key,'email') === true) {
+            if (strpos($key,'email') !== false) {
               $save_data['name'] = $val[0];
               break;
             }
