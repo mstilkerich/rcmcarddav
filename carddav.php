be166468681ecef79b75db727ad6c5c5a5eafae2
Brock Tice <brock@brocktice.com>
2015-06-07 14:39:13 -0400

    Fixes to correct database init on sqlite3.

--- carddav.php
+++ carddav.php
@@ -55,6 +55,18 @@ class carddav extends rcube_plugin
 			$qmarks .= ",?";
 		}
 
+		# first initialize the carddav_migrations table if it doesn't exist.
+		$config = rcmail::get_instance()->config;
+		$query = file_get_contents(dirname(__FILE__)."/dbinit/".$db_backend.".sql");
+		if (strlen($query) > 0){
+				$query = str_replace("TABLE_PREFIX", $config->get('db_prefix', ""), $query);
+				$dbh->query($query);
+				write_log("carddav", "Processed initialization of carddav_migrations table");
+			} else {
+				write_log("carddav", "Can't find migration: /dbinit/".$db_backend.".sql");
+			}
+
+		
 		$dbh->set_option('ignore_key_errors', true);
 		$sql_result = $dbh->query('SELECT * FROM '.
 			get_table_name('carddav_migrations') .
@@ -69,17 +81,22 @@ class carddav extends rcube_plugin
 		}
 		$dbh->set_option('ignore_key_errors', null);
 
-		$config = rcmail::get_instance()->config;
 		foreach ($migrations as $migration){
-			$query = file_get_contents(dirname(__FILE__)."/dbmigrations/".$migration."/".$db_backend.".sql");
-			if (strlen($query) > 0){
-				$query = str_replace("TABLE_PREFIX", $config->get('db_prefix', ""), $query);
-				$dbh->query($query);
-				write_log("carddav", "Processed migration: $migration");
-			} else {
-				write_log("carddav", "Can't find migration: /dbmigrations/".$migration."/".$db_backend.".sql");
+			write_log('carddav', "In migrations, migration ".$migration);
+			$queries_raw = file_get_contents(dirname(__FILE__)."/dbmigrations/".$migration."/".$db_backend.".sql");
+			$match_count = preg_match_all('/(.+?;)/s', $queries_raw, $matches);
+			write_log('carddav', 'Found '.$match_count.' matches');
+			if($match_count > 0){
+			    foreach ($matches[0] as $query){
+				if (strlen($query) > 0){
+				    $query = str_replace("TABLE_PREFIX", $config->get('db_prefix', ""), $query);
+				    $dbh->query($query);
+				}
+			    }
+			    $dbh->query("INSERT INTO ".get_table_name("carddav_migrations")." (filename) VALUES (?)", $migration);
+			}else{
+				write_log('carddav', "Did not match any instructions from migration ".$migration);
 			}
-			$dbh->query("INSERT INTO ".get_table_name("carddav_migrations")." (filename) VALUES (?)", $migration);
 		}
 	}
 
