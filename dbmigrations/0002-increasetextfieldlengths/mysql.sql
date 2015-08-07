084a6a21b31148130d96a1563ff2e6bf15379cc7
Benjamin 'blindCoder' Schieder <benjamin@anderdonau.de>
2015-08-07 07:24:46 +0200

    migration to increase field lengths

---
+++ dbmigrations/0002-increasetextfieldlengths/mysql.sql
@@ -0,0 +1,3 @@
+ALTER TABLE TABLE_PREFIXcarddav_addressbooks MODIFY COLUMN `name` VARCHAR(255);
+ALTER TABLE TABLE_PREFIXcarddav_addressbooks MODIFY COLUMN `username` VARCHAR(255);
+ALTER TABLE TABLE_PREFIXcarddav_addressbooks MODIFY COLUMN `presetname` VARCHAR(255);
