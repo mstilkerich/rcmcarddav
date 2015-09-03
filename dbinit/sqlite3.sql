701bfd8214ce6ab0868e4814b7c11b22a8f9726f
Benjamin 'blindCoder' Schieder <blindcoder@scavenger.homeip.net>
2015-09-03 13:15:45 +0200

    remove obsolete dbinit directory

--- dbinit/sqlite3.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS carddav_migrations (
-	ID integer NOT NULL PRIMARY KEY,
-	filename VARCHAR(64) NOT NULL,
-	processed_at TIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-	UNIQUE(filename)
-);
