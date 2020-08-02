39b059d426cf94d03b5a069ee5063654d731b944
Michael Stilkerich <ms@mike2k.de>
2020-08-02 21:06:47 +0200

    Fix migration 0010 for MySQL
    uri in contacts/groups is UNIQUE, and index keys with InnoDB can have a
    maximum of 767 bytes.

--- dbmigrations/0010-increasetextfieldlengths/mysql.sql
+++ dbmigrations/0010-increasetextfieldlengths/mysql.sql
@@ -3,7 +3,9 @@ ALTER TABLE TABLE_PREFIXcarddav_addressbooks MODIFY COLUMN `url` VARCHAR(4095) C
 ALTER TABLE TABLE_PREFIXcarddav_addressbooks MODIFY COLUMN `sync_token` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE TABLE_PREFIXcarddav_contacts MODIFY COLUMN `email` VARCHAR(4095) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE TABLE_PREFIXcarddav_contacts MODIFY COLUMN `uri` VARCHAR(4095) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- uri is part of UNIQUE index, it can have a max key length of 767 bytes
+ALTER TABLE TABLE_PREFIXcarddav_contacts MODIFY COLUMN `uri` VARCHAR(700) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE TABLE_PREFIXcarddav_groups MODIFY COLUMN `vcard` LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE TABLE_PREFIXcarddav_groups MODIFY COLUMN `uri` VARCHAR(4095) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- uri is part of UNIQUE index, it can have a max key length of 767 bytes
+ALTER TABLE TABLE_PREFIXcarddav_groups MODIFY COLUMN `uri` VARCHAR(700) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
