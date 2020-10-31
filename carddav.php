f3b292df7ecc9f55e7d0e7725af0edd777a239cf
Michael Stilkerich <ms@mike2k.de>
2020-10-31 11:10:21 +0100

    Use version name as in git tag (v prefix)

--- carddav.php
+++ carddav.php
@@ -33,7 +33,7 @@ class carddav extends rcube_plugin
      *
      * During development, it is set to the last release and added the suffix +dev.
      */
-    private const PLUGIN_VERSION = '4.0.0+dev';
+    private const PLUGIN_VERSION = 'v4.0.0+dev';
 
     /**
      * Information about this plugin that is queried by roundcube.
