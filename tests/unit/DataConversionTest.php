d21b1a3e5cde551c942a853d350633dfeb056cd0
Michael Stilkerich <ms@mike2k.de>
2020-11-07 18:52:06 +0100

    More unittests for X-ABLabel extension

--- tests/unit/DataConversionTest.php
+++ tests/unit/DataConversionTest.php
@@ -48,28 +48,111 @@ final class DataConversionTest extends TestCase
      */
     public function testCorrectConversionOfVcardToRoundcube(string $vcfFile, string $jsonFile): void
     {
-        $this->assertFileIsReadable($vcfFile);
-        $this->assertFileIsReadable($jsonFile);
+        [ $logger, $db, $abook ] = $this->initStubs();
 
-        $json = file_get_contents($jsonFile);
-        $this->assertNotFalse($json);
-        $saveDataExpected = json_decode($json, true);
-        $this->assertTrue(is_array($saveDataExpected));
-
-        $vcard = VObject\Reader::read(fopen($vcfFile, 'r'));
-        $this->assertInstanceOf(VCard::class, $vcard);
-
-        $abook = $this->createStub(AddressbookCollection::class);
-        $db = $this->createMock(Database::class);
-
-        $logger = TestInfrastructure::$logger;
-        $this->assertInstanceOf(LoggerInterface::class, $logger);
-
-        $dc = new DataConversion("abookId", $db, $logger);
-
+        $dc = new DataConversion("42", $db, $logger);
+        $vcard = $this->readVCard($vcfFile);
+        $saveDataExpected = $this->readJsonArray($jsonFile);
         $result = $dc->toRoundcube($vcard, $abook);
         $saveData = $result["save_data"];
         $this->assertEquals($saveDataExpected, $saveData, "Converted VCard does not result in expected roundcube data");
+    }
+
+    /**
+     * Tests that a new custom label is inserted into the database.
+     */
+    public function testNewCustomLabelIsInsertedToDatabase(): void
+    {
+        [ $logger, $db, $abook ] = $this->initStubs();
+
+        $db->expects($this->once())
+            ->method("insert")
+            ->with(
+                $this->equalTo("xsubtypes"),
+                $this->equalTo(["typename", "subtype", "abook_id"]),
+                $this->equalTo(["email", "SpecialLabel", "42"])
+            )
+            ->will($this->returnValue("49"));
+
+        $dc = new DataConversion("42", $db, $logger);
+        $vcard = $this->readVCard("tests/unit/data/XAbLabel.vcf");
+        $result = $dc->toRoundcube($vcard, $abook);
+    }
+
+    /**
+     * Tests that known custom labels are offered in coltypes.
+     */
+    public function testKnownCustomLabelPresentedToRoundcube(): void
+    {
+        [ $logger, $db, $abook ] = $this->initStubs();
+
+        $db->expects($this->once())
+            ->method("get")
+            ->with(
+                $this->equalTo("42"),
+                $this->equalTo('typename,subtype'),
+                $this->equalTo('xsubtypes'),
+                $this->equalTo(false),
+                $this->equalTo('abook_id')
+            )
+            ->will($this->returnValue([ ["typename" => "email", "subtype" => "SpecialLabel"] ]));
+
+        $dc = new DataConversion("42", $db, $logger);
+        $coltypes = $dc->getColtypes();
+        $this->assertContains("SpecialLabel", $coltypes["email"]["subtypes"], "SpecialLabel not contained in coltypes");
+    }
+
+    /**
+     * Tests that a known custom label is not inserted into the database again.
+     */
+    public function testKnownCustomLabelIsNotInsertedToDatabase(): void
+    {
+        [ $logger, $db, $abook ] = $this->initStubs();
+
+        $db->expects($this->once())
+            ->method("get")
+            ->with(
+                $this->equalTo("42"),
+                $this->equalTo('typename,subtype'),
+                $this->equalTo('xsubtypes'),
+                $this->equalTo(false),
+                $this->equalTo('abook_id')
+            )
+            ->will($this->returnValue([ ["typename" => "email", "subtype" => "SpecialLabel"] ]));
+        $db->expects($this->never())
+            ->method("insert");
+
+        $dc = new DataConversion("42", $db, $logger);
+        $vcard = $this->readVCard("tests/unit/data/XAbLabel.vcf");
+        $result = $dc->toRoundcube($vcard, $abook);
+    }
+
+    private function initStubs(): array
+    {
+        $logger = TestInfrastructure::$logger;
+        $this->assertInstanceOf(LoggerInterface::class, $logger);
+        $abook = $this->createStub(AddressbookCollection::class);
+        $db = $this->createMock(Database::class);
+
+        return [ $logger, $db, $abook ];
+    }
+
+    private function readVCard(string $vcfFile): VCard
+    {
+        $this->assertFileIsReadable($vcfFile);
+        $vcard = VObject\Reader::read(fopen($vcfFile, 'r'));
+        $this->assertInstanceOf(VCard::class, $vcard);
+        return $vcard;
+    }
+
+    private function readJsonArray(string $jsonFile): array
+    {
+        $this->assertFileIsReadable($jsonFile);
+        $json = file_get_contents($jsonFile);
+        $this->assertNotFalse($json);
+        $phpArray = json_decode($json, true);
+        $this->assertTrue(is_array($phpArray));
+        return $phpArray;
     }
 }
 
