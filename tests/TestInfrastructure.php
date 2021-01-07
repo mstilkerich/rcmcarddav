f29fc19cebe71562a5d9a5fe3f28f4dc04af7ff9
Michael Stilkerich <ms@mike2k.de>
2021-01-07 17:44:42 +0100

    Add work in progress function to fix build

--- tests/TestInfrastructure.php
+++ tests/TestInfrastructure.php
@@ -61,6 +61,53 @@ final class TestInfrastructure
         TestCase::assertIsString($content, "$file could not be read");
         return $content;
     }
+
+    /**
+     * Transforms a list of rows in associative array form to a list form.
+     *
+     * In other words, transforms the rows as returned by AbstractDatabase::get() to the format used internally.
+     *
+     * The function asserts that all fields listed in $cols actually have an entry in the provided associative rows.
+     *
+     * The order of rows is preserved.
+     *
+     * @param list<string> $cols The list of columns that defines fields and their order in the result rows.
+     * @param list<array<string,?string>> $assocRows The associative row set where column names are keys.
+     * @param bool $extraIsError If true, an assertion is done on that the associative rows contain now extra fields
+     *                           that are not listed in $cols. If false, such fields will be discarded.
+     * @return list<list<?string>>
+     */
+    public static function xformDatabaseResultToRowList(array $cols, array $assocRows, bool $extraIsError): array
+    {
+        $result = [];
+
+        foreach ($assocRows as $aRow) {
+            $lRow = [];
+
+            foreach ($cols as $col) {
+                TestCase::assertArrayHasKey($col, $aRow, "cols requires field not existing in associative row");
+                $lRow[] = $aRow[$col];
+                unset($aRow[$col]);
+            }
+
+            $result[] = $lRow;
+            if ($extraIsError) {
+                TestCase::assertEmpty($aRow, "Associative row has fields not listed in cols");
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Compares two database rows.
+     *
+     * One row is given in the format as returned by AbstractDatabase::get() (associative array).
+     * The other is given as separate arrays for columns and associated data.
+     */
+    public static function compareRow(): void
+    {
+    }
 }
 
 // vim: ts=4:sw=4:expandtab:fenc=utf8:ff=unix:tw=120:ft=php
