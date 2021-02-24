3682b87a3950d97da146a3325e3cc987944a7f71
Michael Stilkerich <ms@mike2k.de>
2021-02-24 07:29:11 +0100

    Add/update copyright headers

--- tests/autoload_defs.php
+++ tests/autoload_defs.php
@@ -1,5 +1,27 @@
 <?php
 
+/*
+ * RCMCardDAV - CardDAV plugin for Roundcube webmail
+ *
+ * Copyright (C) 2011-2021 Benjamin Schieder <rcmcarddav@wegwerf.anderdonau.de>,
+ *                         Michael Stilkerich <ms@mike2k.de>
+ *
+ * This file is part of RCMCardDAV.
+ *
+ * RCMCardDAV is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * RCMCardDAV is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RCMCardDAV. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 define('INSTALL_PATH', realpath(__DIR__ . '/../roundcubemail') . '/');
 
 // vim: ts=4:sw=4:expandtab:fenc=utf8:ff=unix:tw=120
--- tests/dbinterop/autoload.php
+++ tests/dbinterop/autoload.php
@@ -1,5 +1,27 @@
 <?php
 
+/*
+ * RCMCardDAV - CardDAV plugin for Roundcube webmail
+ *
+ * Copyright (C) 2011-2021 Benjamin Schieder <rcmcarddav@wegwerf.anderdonau.de>,
+ *                         Michael Stilkerich <ms@mike2k.de>
+ *
+ * This file is part of RCMCardDAV.
+ *
+ * RCMCardDAV is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * RCMCardDAV is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RCMCardDAV. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 include_once __DIR__ . '/../autoload.php';
 
 $classLoader = new \Composer\Autoload\ClassLoader();
