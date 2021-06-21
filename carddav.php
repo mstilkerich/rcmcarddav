f18b1310e168de214df20d4ef778ce05dfb0f9bc
Michael Stilkerich <ms@mike2k.de>
2021-06-21 17:53:44 +0200

    Reset version to development

--- carddav.php
+++ carddav.php
@@ -68,7 +68,7 @@ class carddav extends rcube_plugin
      *
      * During development, it is set to the last release and added the suffix +dev.
      */
-    private const PLUGIN_VERSION = 'v4.1.2';
+    private const PLUGIN_VERSION = 'v4.2.0+dev';
 
     /**
      * Information about this plugin that is queried by roundcube.
