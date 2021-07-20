6fbe52d168a21a82112033ee87f16579b2f21d3f
Michael Stilkerich <ms@mike2k.de>
2021-07-20 15:48:07 +0200

    Adapt initial schema for SQLite3

--- dbmigrations/INIT-currentschema/sqlite3.sql
+++ dbmigrations/INIT-currentschema/sqlite3.sql
@@ -1,23 +1,40 @@
--- table to store the configured address books
-CREATE TABLE IF NOT EXISTS TABLE_PREFIXcarddav_addressbooks (
+-- table to store the configured accounts
+CREATE TABLE IF NOT EXISTS TABLE_PREFIXcarddav_accounts (
 	id           integer NOT NULL PRIMARY KEY,
 	name         TEXT NOT NULL,
 	username     TEXT NOT NULL,
 	password     TEXT NOT NULL,
-	url          TEXT NOT NULL,
-	active       TINYINT UNSIGNED NOT NULL DEFAULT 1,
+	url          TEXT,
 	user_id      integer NOT NULL,
-	last_updated BIGINT NOT NULL DEFAULT 0,  -- time stamp (seconds since epoch) of the last update of the local database
-	refresh_time INT NOT NULL DEFAULT 3600, -- time span (seconds) after that the local database will be refreshed, default 1h
-	sync_token   TEXT NOT NULL DEFAULT '', -- sync-token the server sent us for the last sync
+	last_discovered BIGINT NOT NULL DEFAULT 0,  -- time stamp (seconds since epoch) of the addressbooks were last discovered
+	rediscover_time INT NOT NULL DEFAULT 86400, -- time span (seconds) after that the addressbooks will be rediscovered, default 1d
 
 	presetname   TEXT,                  -- presetname
-
-	use_categories TINYINT NOT NULL DEFAULT 0,
 
 	-- not enforced by sqlite < 3.6.19
 	FOREIGN KEY(user_id) REFERENCES TABLE_PREFIXusers(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+CREATE INDEX TABLE_PREFIXcarddav_accounts_user_id_idx ON TABLE_PREFIXcarddav_accounts(user_id);
+
+-- table to store the configured address books
+CREATE TABLE IF NOT EXISTS TABLE_PREFIXcarddav_addressbooks (
+	id           integer NOT NULL PRIMARY KEY,
+	name         TEXT NOT NULL,
+	url          TEXT NOT NULL,
+	active       TINYINT UNSIGNED NOT NULL DEFAULT 1,
+	last_updated BIGINT NOT NULL DEFAULT 0,  -- time stamp (seconds since epoch) of the last update of the local database
+	refresh_time INT NOT NULL DEFAULT 3600, -- time span (seconds) after that the local database will be refreshed, default 1h
+	sync_token   TEXT NOT NULL DEFAULT '', -- sync-token the server sent us for the last sync
+
+	use_categories TINYINT NOT NULL DEFAULT 0,
+	discovered   TINYINT NOT NULL DEFAULT 1,
+
+	account_id   integer NOT NULL,
+
+	-- not enforced by sqlite < 3.6.19
+	FOREIGN KEY(account_id) REFERENCES TABLE_PREFIXcarddav_accounts(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+CREATE INDEX TABLE_PREFIXcarddav_addressbooks_account_id_idx ON TABLE_PREFIXcarddav_addressbooks(account_id);
 
 CREATE TABLE IF NOT EXISTS TABLE_PREFIXcarddav_contacts (
 	id           integer NOT NULL PRIMARY KEY,
