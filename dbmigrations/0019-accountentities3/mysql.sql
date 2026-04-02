999dfe1337c06916941598e10b2dad56b71dd943
Michael Stilkerich <ms@mike2k.de>
2026-04-02 22:29:15 +0200

    Fix migration 0019: determine foreign key constraint name dynamically
    With mariadb 12.2.2, we encountered firstly an issue where the
    constraint was named differently from what it used to be, so the
    hardcoded name won't work anymore.

--- dbmigrations/0019-accountentities3/mysql.sql
+++ dbmigrations/0019-accountentities3/mysql.sql
@@ -13,10 +13,19 @@ ALTER TABLE TABLE_PREFIXcarddav_addressbooks DROP username;
 ALTER TABLE TABLE_PREFIXcarddav_addressbooks DROP password;
 ALTER TABLE TABLE_PREFIXcarddav_addressbooks DROP presetname;
 
--- The below constraint name is hard-coded for simplicity; it is auto-selected by MySQL
--- so in theory it might not have that name and the query would fail. However, I cannot
--- think of why the name would be different.
--- In case it is not, the actual name can be found via the following query for manual cleanup:
--- SELECT constraint_name from information_schema.key_column_usage where table_name='carddav_addressbooks' and column_name='user_id' and constraint_schema=DATABASE() and referenced_table_name='users' and referenced_column_name = 'user_id';
-ALTER TABLE TABLE_PREFIXcarddav_addressbooks DROP FOREIGN KEY carddav_addressbooks_ibfk_1;
+-- We need to drop the foreign key constraint for user_id first before we can drop the column.
+-- As we do not know the name, we need to figure it out first.
+SET @fk_name = NULL;
+
+SELECT CONSTRAINT_NAME INTO @fk_name FROM information_schema.KEY_COLUMN_USAGE WHERE table_name='TABLE_PREFIXcarddav_addressbooks' AND column_name='user_id' AND referenced_table_name IS NOT NULL LIMIT 1;
+
+SET @exec_query = IF(@fk_name IS NOT NULL,
+    CONCAT('ALTER TABLE TABLE_PREFIXcarddav_addressbooks DROP FOREIGN KEY `', @fk_name, '`'),
+    'SELECT "No FK found, skipping"');
+
+PREPARE stmt FROM @exec_query;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+-- ALTER TABLE TABLE_PREFIXcarddav_addressbooks DROP FOREIGN KEY carddav_addressbooks_ibfk_1;
 ALTER TABLE TABLE_PREFIXcarddav_addressbooks DROP user_id;
